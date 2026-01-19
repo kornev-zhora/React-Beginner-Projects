298bd5f5456e5330abacc08cdf89dcd361da7f23
serega <serega@local.tuf>
2026-01-19 17:47:40 +0200

    001. Installing Router.mp4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Routes from 'routes';
+import {BrowserRouter as Router} from "react-router";
+// import {BrowserRouter as Router} from 'react-router-dom'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+    return (
+        <div className="App">
+            <h3>Welcome here is our app</h3>
+            <Router>
+                <Routes />
+            </Router>
+        </div>
+    )
+};
+
 root.render(
     <React.StrictMode>
         <App />
